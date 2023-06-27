9d0b279220d5a8246375dc69e74a9f93c54ddf75
NevenNabil <neven.nabil201@gmail.com>
2023-06-26 17:20:01 -0700

    betty

--- Getenv.c
+++ Getenv.c
@@ -1,18 +1,20 @@
 #include "AN_shell.h"
-/** _getenv - function receive environmental variable
+/**
+* _getenv - function receive environmental variable
 * @name: name of variable
 *Return:Null
 */
-char *_getenv(const char *name) {
-    size_t namelen = string_length(name);
-    char **envp;
-
-    for (envp = environ; *envp != NULL; envp++) {
-        if (strncmp(*envp, name, namelen) == 0 && (*envp)[namelen] == '=') {
-            return &((*envp)[namelen+1]);
-        }
-    }
-
-    return NULL;
+char *_getenv(const char *name)
+{
+	size_t namelen = string_length(name);
+	char **envp;
+	
+	for (envp = environ; *envp != NULL; envp++)
+	{
+		if (strncmp(*envp, name, namelen) == 0 && (*envp)[namelen] == '=')
+		return (&((*envp)[namelen + 1]));
+	}
+}
+	return (NULL);
 }
 
